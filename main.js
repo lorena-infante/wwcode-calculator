0235c58cf50e2d2b197acfda367d1416870803c9
Lorena Infante <m.lorenainfanter@gmail.com>
2023-11-07 12:10:34 -0300

    index and main only with js prompt

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//output on screen
+/* //output on screen
 let totalResult = document.getElementById('result');
 let summary = document.getElementById('summary');
 let summDisplay;
@@ -30,14 +30,20 @@ const equals = document.getElementById('equals');
 //function addNumberToScreen
 
 //anonym functions
-
+//aux numeric variable
+let auxNumber;
+let auxOperator;
+let summArray = [];
 five.addEventListener('click', () => {
     const stringToNumber = Number (five.innerHTML);
     console.log(`this is the number: ${stringToNumber}`);
-    //  summArray.push(stringToNumber);
-    //  joinedSumArray = summArray.join('');    
+    summArray.push(stringToNumber);
+    joinedSumArray = Number(summArray.join(''));    
     //return stringToNumber and push
-    summary.innerHTML = `${stringToNumber}`;
+    summary.innerHTML = `${joinedSumArray}`;
+    console.log(`${joinedSumArray}`);
+    
+    
    
 })
 
@@ -46,3 +52,42 @@ add.addEventListener('click', () => {
     summary.innerHTML = `${add.innerHTML}`;
     console.log(add.innerHTML);
 })
+ */
+
+//only js
+let promptNumberOne = prompt(`Escriba el primer número a operar`);
+let promptNumberTwo = prompt(`Escriba el segundo número a operar`);
+let promptOperation = prompt(`Escriba una operación a realizar:
+"+", "-", "*", "/", "reset" `);
+let nextOperation = prompt(`¿Desea realizar otra operación? si/no`);
+
+let result;
+const ADD = '+';
+const SUBST = '-';
+const MULTIPLY = '*';
+const DIVIDE = '/';
+const RESET = 'reset';
+const YES = 'si';
+const NO = 'no';
+
+//Saving added nums
+
+//choose operator
+switch(promptOperation){
+    case ADD:
+        result = parseFloat(promptNumberOne) + parseFloat(promptNumberTwo);
+        alert(`Esta es la suma de los dos números ${result}`);
+        break;
+    case SUBST:
+        result = parseFloat(promptNumberOne)- parseFloat(promptNumberTwo);
+        alert(`Esta es la resta de los dos números ${result}`);
+        break;
+    case MULTIPLY:
+        result = parseFloat(promptNumberOne) * parseFloat(promptNumberTwo);
+        alert(`Esta es la multiplicación de los dos números ${result}`);
+        break;
+    case DIVIDE:
+        result = parseFloat(promptNumberOne) / parseFloat(promptNumberTwo);
+        alert(`Esta es la división de los dos números ${result}`);
+        break;
+}
